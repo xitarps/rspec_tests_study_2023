8ec4fb1b90781508aec126ea1c900f576f527865
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-23 23:01:38 -0300

    Refactor with subject

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -4,27 +4,27 @@ describe Calculator do
   describe "#sum" do
     context 'with positive numbers' do
       it 'should return positive' do
-        result = Calculator.new.sum(5 ,6)
+        result = subject.sum(5 ,6)
 
         expect(result).to be_eql(11)
       end
     end
     context 'with negative numbers' do
       it 'should return negative' do
-        result = Calculator.new.sum(-5 ,-7)
+        result = subject.sum(-5 ,-7)
 
         expect(result).to be_eql(-12)
       end
     end
     context 'with positive and negative numbers' do
       it 'should return positive' do
-        result = Calculator.new.sum(-5 ,8)
+        result = subject.sum(-5 ,8)
 
         expect(result).to be_eql(3)
       end
 
       it 'should return negative' do
-        result = Calculator.new.sum(-5 ,3)
+        result = subject.sum(-5 ,3)
 
         expect(result).to be_eql(-2)
       end
