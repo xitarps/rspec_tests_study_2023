575de93139c090c5cffcc71379bf4df1f86362c3
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-24 22:59:53 -0300

    Add comparison matchers

---
+++ spec/matchers/comparison/comparison_spec.rb
@@ -0,0 +1,27 @@
+describe 'Comparison Matchers' do
+  it '#equal - test if same object' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).to equal(x)
+    expect(x).not_to equal(y)
+  end
+
+  it '#be - test if same object' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).to equal(x)
+    expect(x).not_to equal(y)
+  end
+
+  it '#eql - test if same value' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).to eql(y)
+  end
+
+  it '#eq - test if same value' do
+    x = 'ruby'
+    y = 'ruby'
+    expect(x).to eq(y)
+  end
+end
