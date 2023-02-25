39137e93565c597dde2aa95415474fab46baf0d8
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-24 23:32:05 -0300

    Add comparison matchers and fix old comparison file to equality matchers

--- spec/matchers/comparison/comparison_spec.rb
+++ spec/matchers/comparison/comparison_spec.rb
@@ -1,27 +1,49 @@
-describe 'Comparison Matchers' do
-  it '#equal - test if same object' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to equal(x)
-    expect(x).not_to equal(y)
+describe 'Comaprison Matchers' do
+  it '>' do
+    expect(5).to be > 1
   end
 
-  it '#be - test if same object' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to equal(x)
-    expect(x).not_to equal(y)
+  it '<' do
+    expect(5).to be < 6
   end
 
-  it '#eql - test if same value' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to eql(y)
+  it '>=' do
+    expect(5).to be >= 4
   end
 
-  it '#eq - test if same value' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to eq(y)
+  it '<=' do
+    expect(5).to be <= 6
+  end
+
+  it 'be_between - inclusive' do
+    expect(4).to be_between(3,5).inclusive
+    expect(5).to be_between(3,5).inclusive
+    expect(3).to be_between(3,5).inclusive
+  end
+
+  it 'be_between - exclusive' do
+    expect(4).to be_between(3,5).exclusive
+    expect(5).not_to be_between(3,5).exclusive
+    expect(3).not_to be_between(3,5).exclusive
+  end
+
+  it 'match - /REGEX/' do
+    expect('x@gmail.com').to match(/[a-zA-Z]+@[a-zA-Z]+/)
+  end
+
+  it 'start_with' do
+    expect('simple phrase').to start_with('simple')
+    expect([1,2,3]).to start_with 1
+  end
+
+  it 'end_with' do
+    expect('simple phrase').to end_with('phrase')
+    expect([1,2,3]).to end_with 3
+  end
+
+  it '<=>' do
+    expect(3 <=> 3).to be 0
+    expect(3 <=> 1).to be 1
+    expect(1 <=> 3).to be -1
   end
 end
