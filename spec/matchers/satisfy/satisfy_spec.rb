0fdf81153974c80127fdf61183146b4ad2c1afa6
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 19:17:54 -0300

    Add satisfy matchers

--- spec/matchers/satisfy/satisfy_spec.rb
+++ spec/matchers/satisfy/satisfy_spec.rb
@@ -3,8 +3,8 @@ describe "Satisfy Matcher" do
     it { expect(10).to satisfy {|x| x % 2 == 0 } }
   end
 
-  context 'a random number from 1 to 10' do
+  context 'a random number from 1 to 1000' do
     def math_magic(x) = ((((x*2)*5)/x)-7) == 3
-    it { expect([1, 2, 3, 4, 5, 6, 7 ,8 ,9, 10].sample).to satisfy('applying mathmagic will always return 3'){ |x| math_magic(x) } }
+    it { expect((1..1000).to_a).to all satisfy('applying mathmagic will always return 3'){ |x| math_magic(x) } }
   end
 end
