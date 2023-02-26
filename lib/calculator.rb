1b701aa06869f6d33bee1731640bd430052cc0f5
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 01:30:16 -0300

    Add errors/exceptions matchers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -5,4 +5,8 @@ class Calculator
   def sum(a, b)
     a + b
   end
+
+  def div(a, b)
+    a / b
+  end
 end
--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,34 +1,49 @@
 require 'calculator'
 
 describe Calculator, ' - Classe calculadora' do
-  subject(:calc) { described_class.new } 
+  subject(:calc) { described_class.new }
+
   describe "#sum" do
     context 'with positive numbers' do
       it 'should return positive' do
-        result = calc.sum(5 ,6)
+        result = calc.sum(5, 6)
 
         expect(result).to be_eql(11)
       end
     end
+
     context 'with negative numbers' do
       it 'should return negative' do
-        result = calc.sum(-5 ,-7)
+        result = calc.sum(-5, -7)
 
         expect(result).to be_eql(-12)
       end
     end
+
     context 'with positive and negative numbers' do
       it 'should return positive' do
-        result = calc.sum(-5 ,8)
+        result = calc.sum(-5, 8)
 
         expect(result).to be_eql(3)
       end
 
       it 'should return negative' do
-        result = calc.sum(-5 ,3)
+        result = calc.sum(-5, 3)
 
         expect(result).to be_eql(-2)
       end
+    end
+  end
+
+  describe "#div" do
+    it 'should return exception on division by zero' do
+      # scenario = ->{ calc.div(2, 0) }
+      # expect(scenario).to raise_exception(ZeroDivisionError)
+      expect{ calc.div(2, 0) }.to raise_exception(ZeroDivisionError)
+      expect{ calc.div(2, 0) }.to raise_error(ZeroDivisionError)
+      expect{ calc.div(2, 0) }.to raise_error('divided by 0')
+      expect{ calc.div(2, 0) }.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect{ calc.div(2, 0) }.to raise_error(/divided/)
     end
   end
 end
