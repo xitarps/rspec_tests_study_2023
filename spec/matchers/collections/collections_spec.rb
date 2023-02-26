fb68d1c6ce12da62f0ef11e5d2c361bec389ebb8
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 02:08:15 -0300

    Add collections matchers

--- spec/matchers/collections/collections_spec.rb
+++ spec/matchers/collections/collections_spec.rb
@@ -1,4 +1,5 @@
 describe "Collections matchers" do
   subject { [1, 7, 9] }
   it { is_expected.to all (be_odd).and be_an(Integer)}
+  it { expect(['Rails', 'Ruby']).to all (include('R').and be_a(String))}
 end
--- spec/matchers/expectation_compositions/expectation_compositions_spec.rb
+++ spec/matchers/expectation_compositions/expectation_compositions_spec.rb
@@ -2,5 +2,5 @@ describe 'Expectation Compositions Matchers' do
   subject { 'Ruby on Rails' }
 
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
-  it { expect(%w(uva maçã pêra).sample).to be_eql('maça').or be_eql('uva').or be_eql('pêra') }
+  it { expect(%w(uva maçã pêra).sample).to be_eql('maçã').or be_eql('uva').or be_eql('pêra') }
 end
