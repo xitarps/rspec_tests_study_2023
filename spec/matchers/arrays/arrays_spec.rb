e6471c3ffae6e0df2442ed785cb5e5b36981594c
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 01:41:22 -0300

    Add arrays matchers

---
+++ spec/matchers/arrays/arrays_spec.rb
@@ -0,0 +1,14 @@
+describe "Arrays Matchers" do
+  subject { [1, 2, 3, 4] }
+  it '#include' do
+    expect(subject).to include(3)
+    expect(subject).not_to include(5)
+  end
+  it '#match_array' do
+    expect(subject).to match_array([1, 2, 3, 4])
+  end
+  it '#contain_exactly' do
+    expect(subject).to contain_exactly(1, 2, 3, 4)
+    expect(subject).to contain_exactly(4, 3, 2, 1)
+  end
+end
