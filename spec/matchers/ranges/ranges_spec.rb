66f2a9a6cc4943d332f1c0233801b2b510f175e2
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 01:47:55 -0300

    Add one-liner syntax

--- spec/matchers/ranges/ranges_spec.rb
+++ spec/matchers/ranges/ranges_spec.rb
@@ -4,4 +4,7 @@ describe "Ranges Macthers" do
     expect(subject).to cover(2, 5)
     expect(subject).not_to cover(0, 6)
   end
+
+  it { is_expected.to cover(3) }
+  it { is_expected.not_to cover(9) }
 end
