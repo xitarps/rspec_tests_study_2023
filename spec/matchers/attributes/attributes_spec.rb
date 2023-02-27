af479830e0ac87f609ce8ebbbdfdf1142f4aba78
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 21:31:32 -0300

    Add let helper

--- spec/matchers/attributes/attributes_spec.rb
+++ spec/matchers/attributes/attributes_spec.rb
@@ -1,13 +1,16 @@
 require 'person'
 
 describe Person, ' - attributes' do
+  let(:person){ Person.new(name: 'Xita', age: 32) }
+  # let!(:person){ Person.new(name: 'Xita', age: 32) }
+
   it 'have_attributes - default values' do
     expect(subject).to have_attributes(name: 'unknow', age: 0)
   end
 
   it 'have_attributes - with a set of values' do
-    subject = Person.new(name: 'Xita', age: 32)
-    expect(subject).to have_attributes(name: 'Xita', age: 32)
-    expect(subject).to have_attributes(name: starting_with('X'), age: (be > 31))
+    # subject = Person.new(name: 'Xita', age: 32)
+    expect(person).to have_attributes(name: 'Xita', age: 32)
+    expect(person).to have_attributes(name: starting_with('X'), age: (be > 31))
   end
 end
