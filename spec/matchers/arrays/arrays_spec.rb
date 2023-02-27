3459c8986eb55b0a396f5e6ed52b7b083904ab49
Rodrigo - Xita rps <xitarps@gmail.com>
2023-02-26 22:13:54 -0300

    Add Negativate Matcher

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,8 +1,11 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe "Arrays Matchers" do
   subject { [1, 2, 3, 4] }
   it '#include' do
     expect(subject).to include(3)
     expect(subject).not_to include(5)
+    expect(subject).to exclude(5)
   end
   it '#match_array' do
     expect(subject).to match_array([1, 2, 3, 4])
