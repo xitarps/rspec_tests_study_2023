7d501c83045f1244d24b4d19977734217a1aa12d
Rodrigo - Xita rps <xitarps@gmail.com>
2023-03-02 00:20:46 -0300

    Add tag filters

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,16 +1,17 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe "Arrays Matchers" do
+describe "Arrays Matchers", type: 'collection' do
+# describe "Arrays Matchers", collection: true do
   subject { [1, 2, 3, 4] }
   it '#include' do
     expect(subject).to include(3)
     expect(subject).not_to include(5)
     expect(subject).to exclude(5)
   end
-  it '#match_array' do
+  it '#match_array', :slow do
     expect(subject).to match_array([1, 2, 3, 4])
   end
-  it '#contain_exactly' do
+  it '#contain_exactly', slow: true do
     expect(subject).to contain_exactly(1, 2, 3, 4)
     expect(subject).to contain_exactly(4, 3, 2, 1)
   end
--- spec/matchers/collections/collections_spec.rb
+++ spec/matchers/collections/collections_spec.rb
@@ -1,4 +1,6 @@
-describe "Collections matchers" do
+describe "Collections matchers", :collection do           # rspec-t collection
+# describe "Collections matchers", type: 'collection' do  # rspec type:collection
+# describe "Collections matchers", collection: true do    # rspec -t collection
   subject { [1, 7, 9] }
   it { is_expected.to all (be_odd).and be_an(Integer)}
   it { expect(['Rails', 'Ruby']).to all (include('R').and be_a(String))}
